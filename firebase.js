59502c69999a2b582010986d273c9e330a4e1951
lynar13 <61920408+lynar13@users.noreply.github.com>
2023-01-19 23:08:33 +0100

    Add files via upload
    first commit

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDNzcD0kqjd80V--wy2F-J6fwmSimi06MA",
+    authDomain: "newbiz-b183b.firebaseapp.com",
+    projectId: "newbiz-b183b",
+    storageBucket: "newbiz-b183b.appspot.com",
+    messagingSenderId: "9863569857",
+    appId: "1:9863569857:web:4d3cbd2e54c1b81d35cf40",
+  };
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  
